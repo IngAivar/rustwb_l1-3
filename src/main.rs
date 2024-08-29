386160dfe154e37e01917e828b7c7c35783f617b
IngAivar <MiniGan067@yandex.ru>
2024-08-29 15:16:09 +0300

    new branch

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 use std::thread;
 use std::sync::mpsc;
-use std::sync::Arc;
 
 fn main() {
-    let n = 100; // Размер массива
-    let numbers: Vec<i32> = (1..=n).collect();
+    let n = 12; // Размер массива
+    let _numbers: Vec<i32> = (1..=n).collect();
 
     // Создаем канал для передачи результатов
     let (tx, rx) = mpsc::channel();
+    let mut handles = vec![];
 
-    // Преобразуем вектор в Arc для многократного использования в потоках
-    let numbers = Arc::new(numbers);
-
-    // Создаем потоки для расчета квадратов
-    for num in numbers {
+    // Создаем потоки для расчета сумм квадратов
+    for num in _numbers {
         let tx = tx.clone();
-        let num = *num;
-        thread::spawn(move || {
+
+        let handle = thread::spawn(move || {
             let square = num * num + num * num;
             tx.send(square).unwrap();
         });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
     }
 
     // Считываем результаты из канала и выводим их
     for result in rx {
         println!("{}", result);
     }
+    
 }
