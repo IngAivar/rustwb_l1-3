d900ae76bb88d4f1aaf653daef1519ed411fbe1f
IngAivar <MiniGan067@yandex.ru>
2024-08-29 15:23:05 +0300

    Работает, но неприрывно

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,15 @@ fn main() {
 
     // Создаем канал для передачи результатов
     let (tx, rx) = mpsc::channel();
-    let mut handles = vec![];
 
     // Создаем потоки для расчета сумм квадратов
     for num in _numbers {
         let tx = tx.clone();
 
-        let handle = thread::spawn(move || {
+        thread::spawn(move || {
             let square = num * num + num * num;
             tx.send(square).unwrap();
         });
-        handles.push(handle);
-    }
-    for handle in handles {
-        handle.join().unwrap();
     }
 
     // Считываем результаты из канала и выводим их
